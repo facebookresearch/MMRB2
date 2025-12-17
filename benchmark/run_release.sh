b4cac81f40655e48dd7fadfc9aee0e12e5fbfd09
Yushi-Hu <huyushi1015@gmail.com>
2025-12-17 09:27:39 +0000

    add header

--- benchmark/run_release.sh
+++ benchmark/run_release.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the CC BY-NC 4.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
 # MMRB2 Release Pipeline
 # 
 # This script builds the full MMRB2 dataset by:
